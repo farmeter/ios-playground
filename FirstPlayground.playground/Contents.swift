e44115f2134970f5822fa71f7e0ef092c8ee3eab
farmeter <farmeter@gmail.com>
2020-01-31 14:05:43 +0900

    add func

--- FirstPlayground.playground/Contents.swift
+++ FirstPlayground.playground/Contents.swift
@@ -11,19 +11,22 @@ class MyViewController : UIViewController {
         let view = UIView()
         view.backgroundColor = .white
 
-        let headLabel = UILabel()
-        headLabel.frame = CGRect(x:50, y:100, width: 200, height:30)
-        headLabel.text = "This is head."
-        
         let label = UILabel()
         label.frame = CGRect(x: 150, y: 200, width: 200, height: 20)
         label.text = "Hello World!"
         label.textColor = .black
         
-        view.addSubview(headLabel)
+        view.addSubview(headLabel())
         view.addSubview(label)
         
         self.view = view
+    }
+    
+    func headLabel() -> UILabel {
+        let headLabel = UILabel()
+        headLabel.frame = CGRect(x:50, y:100, width: 200, height:30)
+        headLabel.text = "This is head.."
+        return headLabel
     }
 }
 // Present the view controller in the Live View window
