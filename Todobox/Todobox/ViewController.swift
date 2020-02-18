8d42d7a10358c67c5728f6c379e1c3b19d4ec172
farmeter <farmeter@gmail.com>
2020-02-18 10:44:36 +0900

    mod ViewController

--- Todobox/Todobox/ViewController.swift
+++ Todobox/Todobox/ViewController.swift
@@ -9,12 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+//        self.tableView.backgroundColor = UIColor.red
     }
-
-
+    
+    //TODO FIX
+    //TODO FIX
+    // n번째 섹션에 몇 개의 row가 존재하는지를 반환
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) {
+        return 100
+    }
+    
+    // n번째 섹션의 m번째 row를 그리는데 필요한 셀 반환
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) {
+        let cell = UITableViewCell()
+        cell.textField?.text = "\(indexPath.row)"
+        return cell
+    }
+    
+    
 }
 
