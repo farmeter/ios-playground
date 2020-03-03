564e67c15df06bc3101a567f10b632aa8b5b2a93
farmeter <farmeter@gmail.com>
2020-03-03 10:20:06 +0900

    add test project

---
+++ MyDream/MyDream/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyDream
+//
+//  Created by USER on 03/03/2020.
+//  Copyright © 2020 USER. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
